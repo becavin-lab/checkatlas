4be5e421023cbdfabdbba32945f0accbcad7b060
christophe Becavin <christophebecavin@dhcp34.ipmc.cnrs.fr>
2022-03-03 09:09:09 +0100

    Clean project files

--- setup.py
+++ setup.py
@@ -6,11 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-<<<<<<< HEAD
     read("checkatlas", "VERSION")
-=======
-    >>> read("checkatlas", "VERSION")
->>>>>>> b6ed53fdf89beca99eb88ccb03bd83ed951329f4
     '0.1.0'
     """
 
@@ -34,13 +30,8 @@ def read_requirements(path):
 setup(
     name="checkatlas",
     version=read("checkatlas", "VERSION"),
-<<<<<<< HEAD
-    description="project_description",
-    url="https://github.com/becavin-lab/checkatlas",
-=======
     description="Awesome checkatlas created by becavin-lab",
     url="https://github.com/becavin-lab/checkatlas/",
->>>>>>> b6ed53fdf89beca99eb88ccb03bd83ed951329f4
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     author="becavin-lab",
