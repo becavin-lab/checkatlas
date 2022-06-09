5ec264b3070c5ce4c3de460a40980ab03bdfaa02
christophe Becavin <christophebecavin@dhcp34.ipmc.cnrs.fr>
2022-06-09 18:23:50 +0200

    Prepare checkatlas for discovair

--- tests/test_atlas.py
+++ tests/test_atlas.py
@@ -1,19 +1,24 @@
 import pytest
-
-from checkatlas import atlas
-from checkatlas.atlas import list_atlases
+import scanpy as sc
+from checkatlas.atlas import clean_scanpy_atlas
 
 given = pytest.mark.parametrize
 
 
-@given("fn", [atlas(), list_atlases()])
-def test_parameterized(fn):
-    assert "hello from" in fn()
-
-
-def test_list_atlases():
-    assert list_atlases(".") == "hello from base function"
-
-
 def test_atlas():
-    assert atlas() == "hello from BaseClass"
+    adata = sc.datasets.pbmc68k_reduced()
+    atlas_info = ['PBMC68k','Scanpy','.h5ad','Scanpy module']
+    assert clean_scanpy_atlas(adata, atlas_info)
+
+
+# @given("fn", [atlas(), list_atlases()])
+# def test_parameterized(fn):
+#     assert "hello from" in fn()
+#
+#
+# def test_list_atlases():
+#     assert list_atlases(".") == "hello from base function"
+#
+#
+# def test_atlas():
+#     assert atlas() == "hello from BaseClass"
