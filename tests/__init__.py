c050fe350b2a2da1272e5f3e503fcde72213796f
Christophe Bécavin <becavin.christophe@gmail.com>
2023-07-09 23:17:44 +0200

    Continuous integration 🔄 tests-0.2.15

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,15 +1,1 @@
-from . import (
-    conftest,
-    test_atlas_seurat,
-    test_checkatlas,
-    test_folders,
-    test_metrics,
-)
 
-__all__ = [
-    "conftest",
-    "test_atlas_seurat",
-    "test_checkatlas",
-    "test_folders",
-    "test_metrics",
-]
