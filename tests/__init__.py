b572a542477ee49547a9b2a5dad99843a4cf3aaa
Christophe Bécavin <becavin.christophe@gmail.com>
2023-07-09 21:15:12 +0200

    Clean init.py in tests

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+from . import conftest
+from . import test_atlas_seurat
+from . import test_checkatlas
+from . import test_folders
+from . import test_metrics
+from . import data
+
+__all__ = ["conftest", "test_atlas_seurat", "test_checkatlas",\
+           "test_folders", "test_metrics","data"]
