e1c8a46b86f4e0a5d00f93633ca2b38346dedaa3
Christophe Bécavin <becavin.christophe@gmail.com>
2023-07-10 10:07:46 +0200

    Fix Seurat install

--- tests/data/test_R.py
+++ tests/data/test_R.py
@@ -1,16 +1,26 @@
+import rpy2.robjects as robjects
+import rpy2.robjects.packages as rpackages
+from rpy2.robjects.vectors import StrVector
+
+
 def test_r():
-    print("Test Seurat")
-    # seurat_path = "/Users/christophebecavin/
-    # Documents/testatlas/PAH_675093.rds"
-    #
-    # print(seurat_path)
-    #
-    #
-    # # atlas_seurat.install_library()
-    # rcode = f'readRDS("{seurat_path}")'
-    # print(rcode)
-    # seurat = robjects.r(rcode)
-    # print(seurat)
+    utils = rpackages.importr("utils")
+    # select a mirror for R packages
+    utils.chooseCRANmirror(ind=1)  # select the first mirror in the list
+    # R package names
+    packnames = ("Seurat", "SeuratObject")
+    # Selectively install what needs to be install.
+    # We are fancy, just because we can.
+    names_to_install = [x for x in packnames if not rpackages.isinstalled(x)]
+    if len(names_to_install) > 0:
+        rcode = """Sys.getenv("R_LIBS_USER")"""
+        print(robjects.r(rcode))
+        rcode = """dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)"""
+        print(robjects.r(rcode))
+        # add to the path
+        rcode = """.libPaths(Sys.getenv("R_LIBS_USER"))"""
+        print(robjects.r(rcode))
+        utils.install_packages(StrVector(names_to_install))
 
 
 if __name__ == "__main__":
