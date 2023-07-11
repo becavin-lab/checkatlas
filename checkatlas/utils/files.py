512b7f68ab929146eb600b573cc160c4f6ba7f97
Christophe Bécavin <becavin.christophe@gmail.com>
2023-07-11 10:17:47 +0200

    Add saving to pandas csv the list of atlases found, and checkatlas.read_list_atlases function

--- checkatlas/utils/files.py
+++ checkatlas/utils/files.py
@@ -1,5 +1,9 @@
 import os
 
+import pandas as pd
+
+from checkatlas import checkatlas
+
 from . import folders
 
 
@@ -22,8 +26,57 @@ def get_file_path(
     return csv_path
 
 
-def get_table_atlas_path(checkatlas_path: str) -> str:
+def get_table_scanpy_path(checkatlas_path: str) -> str:
     file_path = os.path.join(
-        folders.get_workingdir(checkatlas_path), "List_atlases.csv"
+        folders.get_workingdir(checkatlas_path), "List_scanpy.csv"
     )
     return file_path
+
+
+def get_table_cellranger_path(checkatlas_path: str) -> str:
+    file_path = os.path.join(
+        folders.get_workingdir(checkatlas_path), "List_cellranger.csv"
+    )
+    return file_path
+
+
+def get_table_seurat_path(checkatlas_path: str) -> str:
+    file_path = os.path.join(
+        folders.get_workingdir(checkatlas_path), "List_seurat.csv"
+    )
+    return file_path
+
+
+def save_list_scanpy(clean_scanpy_list: list, checkatlas_path: str) -> None:
+    df_summary = pd.DataFrame(columns=checkatlas.ATLAS_TABLE_HEADER)
+    for table_info in clean_scanpy_list:
+        df_summary.loc[
+            table_info[checkatlas.ATLAS_NAME_KEY]
+        ] = table_info.values()
+    df_summary.to_csv(
+        get_table_scanpy_path(checkatlas_path), index=False, sep=","
+    )
+
+
+def save_list_cellranger(
+    clean_cellranger_list: list, checkatlas_path: str
+) -> None:
+    df_summary = pd.DataFrame(columns=checkatlas.ATLAS_TABLE_HEADER)
+    for table_info in clean_cellranger_list:
+        df_summary.loc[
+            table_info[checkatlas.ATLAS_NAME_KEY]
+        ] = table_info.values()
+    df_summary.to_csv(
+        get_table_cellranger_path(checkatlas_path), index=False, sep=","
+    )
+
+
+def save_list_seurat(clean_seurat_list: list, checkatlas_path: str) -> None:
+    df_summary = pd.DataFrame(columns=checkatlas.ATLAS_TABLE_HEADER)
+    for table_info in clean_seurat_list:
+        df_summary.loc[
+            table_info[checkatlas.ATLAS_NAME_KEY]
+        ] = table_info.values()
+    df_summary.to_csv(
+        get_table_seurat_path(checkatlas_path), index=False, sep=","
+    )
