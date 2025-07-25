a0cd85faa77cc527a0fc5d78ed2d81ec62ca49b2
Moccia <lucamoccia2004@gmail.com>
2025-07-25 13:51:41 +0200

    Add link to readthedocs

--- checkatlas/metrics/annot/normalized_mutual_info.py
+++ checkatlas/metrics/annot/normalized_mutual_info.py
@@ -2,5 +2,15 @@ from sklearn.metrics import normalized_mutual_info_score
 
 
 def run(annotation, ref_annotation):
+    """
+
+    `NMI readthedocs
+    <https://checkatlas.readthedocs.io/en/latest/metrics/cellannotation/nmi/>`__
+
+
+    :param annotation:
+    :param ref_annotation:
+    :return:
+    """
 
     return normalized_mutual_info_score(annotation, ref_annotation)
