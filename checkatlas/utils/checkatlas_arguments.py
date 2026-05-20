49f79ef70ce7cec826d31e18d649ac805fad4e25
RitwikGanguly <gangulyritwik2003@gmail.com>
2026-05-20 11:44:09 +0200

    updated the annotation

--- checkatlas/utils/checkatlas_arguments.py
+++ checkatlas/utils/checkatlas_arguments.py
@@ -119,16 +119,7 @@ def create_parser():
         "--metric_annot",
         nargs="+",
         type=str,
-        default=[
-            "rand_index",
-            "adj_rand_index",
-            "normalized_mutual_info",
-            "adj_mutual_info",
-            "mutual_info",
-            "fowlkes_mallows",
-            "vmeasure",
-            "isolated_f1_score",
-        ],
+        default=annot.__all__,
         help="Specify the list of annotation metrics to calculate."
         f"   Example: --metric_annot rand_index"
         f"   List of annotation metrics: {annot.__all__}",
